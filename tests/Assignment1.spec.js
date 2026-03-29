19253181396df5027d09f4f678b3ecc962633766
ramyamadarapu <cramya.it12@gmail.com>
2026-03-29 19:41:01 +0530

    welcome

---
+++ tests/Assignment1.spec.js
@@ -0,0 +1,23 @@
+
+import { test, expect } from '@playwright/test';
+
+
+test ('Fill in all deatils ', async ({ page}) => {
+
+await page.goto('https://demoqa.com/text-box')
+
+await page.locator("input[placeholder='Full Name']").fill('test1')
+
+await page.locator("input[placeholder='name@example.com']").fill('test1@gmail.com')  
+
+await page.locator("textarea[placeholder='Current Address']").fill('123, RaymondStreet, IL')
+
+await page.locator("#permanentAddress").fill('123, RaymondStreet, IL')
+
+await page.locator('#submit').click()
+
+await expect(page.locator('p#currentAddress')).toBeVisible({timeout : 15000})
+
+
+})
+
